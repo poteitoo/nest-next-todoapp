429e402e39cd2dbbdb78c41f3b389e9fb7e1dfe1
poteitoo <1105tigery@gmail.com>
2021-09-14 00:47:42 +0900

    [Add] modules & graphql settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,25 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthController } from './auth/auth.controller';
-import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
-  controllers: [AppController, AuthController],
+  imports: [
+    GraphQLModule.forRoot({
+      debug: true,
+      playground: true,
+      autoSchemaFile: './schema.gql',
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    UserModule,
+    AuthModule,
+  ],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
