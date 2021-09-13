39b5f6d54558d87f45f4edad735d873d90607111
poteitoo <1105tigery@gmail.com>
2021-09-14 00:44:13 +0900

    [Add] auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { LocalStrategy } from './strategies/local.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { UserModule } from '../user/user.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { UserService } from 'src/user/user.service';
+import { AuthController } from './auth.controller';
 
 @Module({
-  providers: [AuthService]
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      useFactory: async (configService: ConfigService) => {
+        return {
+          secret: configService.get<string>('JWT_SECRET_KEY'),
+          signOptions: { expiresIn: '600s' },
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy, UserService],
+  exports: [AuthService],
+  controllers: [AuthController],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,36 @@
+import * as bcrypt from 'bcrypt';
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private usersService: UserService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(username: string, pass: string): Promise<any> {
+    const user = this.usersService.findOne(username);
+
+    // if (user && (await bcrypt.compare(user.password, pass))) {
+    if (user && user.password === pass) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+
+  async login(user: any) {
+    const payload = { username: user.username, id: user.id };
+
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+
+  async makeHash(password: string): Promise<string> {
+    const salt = await bcrypt.genSalt();
+    return await bcrypt.hash(password, salt);
+  }
+}
